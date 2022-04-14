51a491790862055914af876d29717922767056bb
Владислав Савченко <vs101ff@gmail.com>
2022-04-14 12:41:02 +0300

    Revert "feat: setup chakra-ui"
    This reverts commit 6a8a89011f5e098c8f69038dc507c6c40c8107bc.

--- app/root.tsx
+++ app/root.tsx
@@ -3,7 +3,6 @@ import type {
   LoaderFunction,
   MetaFunction,
 } from "@remix-run/node";
-import React, { useContext, useEffect } from "react";
 import { json } from "@remix-run/node";
 import {
   Links,
@@ -13,23 +12,11 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-import { withEmotionCache } from "@emotion/react";
-import { ChakraProvider } from "@chakra-ui/react";
-
-import { ServerStyleContext, ClientStyleContext } from "./context";
 
 import { getUser } from "./session.server";
-import theme from "./theme";
 
 export const links: LinksFunction = () => {
-  return [
-    { rel: "preconnect", href: "https://fonts.googleapis.com" },
-    { rel: "preconnect", href: "https://fonts.gstaticom" },
-    {
-      rel: "stylesheet",
-      href: "https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap",
-    },
-  ];
+  return [];
 };
 
 export const meta: MetaFunction = () => ({
@@ -48,60 +35,19 @@ export const loader: LoaderFunction = async ({ request }) => {
   });
 };
 
-interface DocumentProps {
-  children: React.ReactNode;
-}
-
-const Document = withEmotionCache(
-  ({ children }: DocumentProps, emotionCache) => {
-    const serverStyleData = useContext(ServerStyleContext);
-    const clientStyleData = useContext(ClientStyleContext);
-
-    // Only executed on client
-    useEffect(() => {
-      // re-link sheet container
-      emotionCache.sheet.container = document.head;
-      // re-inject tags
-      const tags = emotionCache.sheet.tags;
-      emotionCache.sheet.flush();
-      tags.forEach((tag) => {
-        (emotionCache.sheet as any)._insertTag(tag);
-      });
-      // reset cache to reapply global styles
-      clientStyleData?.reset();
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-
-    return (
-      <html lang="en">
-        <head>
-          <Meta />
-          <Links />
-          {serverStyleData?.map(({ key, ids, css }) => (
-            <style
-              key={key}
-              data-emotion={`${key} ${ids.join(" ")}`}
-              dangerouslySetInnerHTML={{ __html: css }}
-            />
-          ))}
-        </head>
-        <body>
-          {children}
-          <ScrollRestoration />
-          <Scripts />
-          <LiveReload />
-        </body>
-      </html>
-    );
-  }
-);
-
 export default function App() {
   return (
-    <Document>
-      <ChakraProvider theme={theme}>
+    <html lang="en" className="h-full">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body className="h-full">
         <Outlet />
-      </ChakraProvider>
-    </Document>
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
   );
 }
